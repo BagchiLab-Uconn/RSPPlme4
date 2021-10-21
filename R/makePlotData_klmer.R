af2075d8997759127638f2580688842095bfda7c
robertbagchi <robert.bagchi@uconn.edu>
2021-10-21 15:27:26 -0400

    Added option for internal K-function and weights calculation in klmer

--- R/makePlotData_klmer.R
+++ R/makePlotData_klmer.R
@@ -14,12 +14,11 @@
 makePlotData_klmer <- function(x)
 {
   tibble::as_tibble(
-    do.call('rbind', lapply(x, fixef))
+    cbind(distance = as.numeric(names(x)), do.call('rbind', lapply(x, fixef)))
   ) %>% 
     dplyr::rename("Intercept" = "(Intercept)") %>%
-    dplyr::mutate(distance = as.numeric(names(x))) %>%
     tidyr::pivot_longer(cols = - .data$distance, 
                         names_to="term", values_to="estimate", 
                         names_transform=list(term = factor)) %>%
-    mutate(term = relevel(.data$term, "Intercept"))
+    dplyr::mutate(term = relevel(.data$term, "Intercept"))
 }
