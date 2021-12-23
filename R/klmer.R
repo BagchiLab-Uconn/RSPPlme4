1cf423cbd32bad8b22cb0b40eadbb78b531efef9
robertbagchi <robert.bagchi@uconn.edu>
2021-12-23 10:12:47 -0500

    added error for missing variables

--- R/klmer.R
+++ R/klmer.R
@@ -38,9 +38,14 @@ klmer <- function(formula, hyper, weights = NULL,  weights_type = NULL,
   if(min(r) > 0)
     r <- c(0, r) ## Kest requires a 0 distance.
 
+  rhs <- all.vars(update(formula, 0~.))
+  missing <- !(rhs %in% names(hyper))
+  if(any(missing))
+    stop(paste("variable", rhs[missing], "not found\n"))
+         
   lhs <- all.vars(update(formula, .~0))
   if(!(lhs %in% names(hyper)))
-    stop("Response variable is missing from dataset")
+    stop(paste("Response variable", lhs,  "not found\n")
   
   lhs_class <-  class(hyper[,lhs, drop = TRUE][[1]])
   
